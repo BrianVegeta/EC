b28faf297e9f7f0d2fa99cfc1889a5690ec054bd
Nick <dragondrumtest001@gmail.com>
2017-03-10 10:52:58 +0800

    Add index controller action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'api#category'
+  root 'index#index'
   get 'gategory', to: 'api#category'
   get 'hello_world', to: 'hello_world#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
