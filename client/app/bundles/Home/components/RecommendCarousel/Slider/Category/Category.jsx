58a0107a0b53a14b608b775c82c151228bce71a5
Nick <phyala@gmail.com>
2017-03-31 19:19:18 +0800

    Update recommends categories cover image

--- client/app/bundles/Home/components/RecommendCarousel/Slider/Category/Category.jsx
+++ client/app/bundles/Home/components/RecommendCarousel/Slider/Category/Category.jsx
@@ -1,17 +1,16 @@
 import React, { PropTypes } from 'react';
 
-class Category extends React.Component {
-  render() {
-    const { item } = this.props;
-    const { coverUrl, name } = item;
+const propTypes = { item: PropTypes.array.isRequired };
+const Category = (props) => {
+  const { item } = props;
+  const { coverUrl, name } = item;
 
-    return (
-      <div styleName="container">
-        <div styleName="cover" style={{ backgroundImage: `url(${coverUrl})` }} />
-        <div styleName="name">{name}</div>
-      </div>
-    );
-  }
-}
-
+  return (
+    <div styleName="container">
+      <div styleName="cover" style={{ backgroundImage: `url(${coverUrl})` }} />
+      <div styleName="name">{name}</div>
+    </div>
+  );
+};
+Category.propTypes = propTypes;
 export default Category;
