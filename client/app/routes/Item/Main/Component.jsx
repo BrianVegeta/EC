c6ee765b3251db21589f7189611d38c54f9f709e
Nick <phyala@gmail.com>
2017-04-14 19:06:37 +0800

    Add description to item

--- client/app/routes/Item/Main/Component.jsx
+++ client/app/routes/Item/Main/Component.jsx
@@ -3,8 +3,10 @@ import Cover from './Cover';
 import Breadcrumbs from './Breadcrumbs';
 import Title from './Title';
 import TitleFooter from './TitleFooter';
+import Description from './Description';
 
 const title = '微型迷你投影機家庭劇院神器微型迷你投影機家庭劇院神器劇院神器';
+const description = '走到哪拍到哪，到處旅行收藏美麗的的畫面，這台復古相機，跟著我3年時間留下了很多回憶，走到哪拍到哪。面，這台復古相機，跟著我3年時間留下了很多回憶，這台復古相機到處旅行收藏美麗的的畫面，跟著我3年時間留下了很多回憶，走到哪拍到哪'
 const Main = () => (
   <div styleName="container">
     <div styleName="cover">
@@ -18,6 +20,7 @@ const Main = () => (
       <div styleName="title-footer">
         <TitleFooter location="台北市中正區" orderedCount={8} />
       </div>
+      <Description description={description} />
     </div>
   </div>
 );
---
+++ client/app/routes/Item/Main/Description/Component.jsx
@@ -0,0 +1,58 @@
+import React, { PropTypes } from 'react';
+import Truncate from 'react-text-truncate';
+
+const propTypes = {
+  description: PropTypes.string.isRequired,
+};
+class Description extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isDescriptExpened: false,
+    };
+    this.expandDescripton = this.expandDescripton.bind(this);
+  }
+
+  expandDescripton() {
+    this.setState({ isDescriptExpened: true });
+  }
+
+  renderExpendButton() {
+    return (
+      <div styleName="footer">
+        <button styleName="showall-btn" onClick={this.expandDescripton}>
+          <span styleName="showall-text">顯示全部描述</span>
+        </button>
+      </div>
+    );
+  }
+
+  renderTruncated() {
+    const { description } = this.props;
+    return (
+      <Truncate
+        line={3}
+        truncateText="…"
+        text={description}
+      />
+    );
+  }
+
+  render() {
+    const { description } = this.props;
+    const { isDescriptExpened } = this.state;
+    return (
+      <div styleName="container">
+        <div styleName="header">
+          <h2>物品描述</h2>
+        </div>
+        <div styleName="body">
+          { isDescriptExpened ? description : this.renderTruncated() }
+        </div>
+        { !isDescriptExpened && this.renderExpendButton() }
+      </div>
+    );
+  }
+}
+Description.propTypes = propTypes;
+export default Description;
