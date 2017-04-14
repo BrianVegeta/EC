cd9c66c1ec2870b0ab8eb27cf7862edacafa6418
Nick <phyala@gmail.com>
2017-04-14 19:42:34 +0800

    Fix description font size and add empty tags

--- client/app/routes/Item/Main/Component.jsx
+++ client/app/routes/Item/Main/Component.jsx
@@ -4,6 +4,7 @@ import Breadcrumbs from './Breadcrumbs';
 import Title from './Title';
 import TitleFooter from './TitleFooter';
 import Description from './Description';
+import Tags from './Tags';
 
 const title = '微型迷你投影機家庭劇院神器微型迷你投影機家庭劇院神器劇院神器';
 const description = '走到哪拍到哪，到處旅行收藏美麗的的畫面，這台復古相機，跟著我3年時間留下了很多回憶，走到哪拍到哪。面，這台復古相機，跟著我3年時間留下了很多回憶，這台復古相機到處旅行收藏美麗的的畫面，跟著我3年時間留下了很多回憶，走到哪拍到哪'
@@ -21,6 +22,7 @@ const Main = () => (
         <TitleFooter location="台北市中正區" orderedCount={8} />
       </div>
       <Description description={description} />
+      <Tags />
     </div>
   </div>
 );
---
+++ client/app/routes/Item/Main/Tags/Component.jsx
@@ -0,0 +1,16 @@
+import React, { PropTypes } from 'react';
+
+const Tags = () => (
+  <div styleName="container">
+    <div styleName="label">
+      <div styleName="text">#投影機投影機投影機投影機</div>
+    </div>
+    <div styleName="label">
+      <div styleName="text">#投影機投影機投影機投影機</div>
+    </div>
+    <div styleName="label">
+      <div styleName="text">#投影機投影機投影機投影機</div>
+    </div>
+  </div>
+);
+export default Tags;
