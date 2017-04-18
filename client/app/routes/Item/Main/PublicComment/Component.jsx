3772d1c1d3fcbe23ed957258746ba12a7303c78a
Nick <phyala@gmail.com>
2017-04-18 12:21:17 +0800

    Restructure item comment row

--- client/app/routes/Item/Main/PublicComment/Component.jsx
+++ client/app/routes/Item/Main/PublicComment/Component.jsx
@@ -1,21 +1,30 @@
 import React, { PropTypes } from 'react';
+import { Scrollbars } from 'react-custom-scrollbars';
+import CommentRow from './CommentRow';
 
 class PublicComment extends React.Component {
+
+  static rScrollBar({ style, ...props }) {
+    const thumbStyle = { backgroundColor: 'rgb(78, 76, 76)' };
+    return <div style={{ ...style, ...thumbStyle }} {...props} />;
+  }
+
   constructor(props) {
     super(props);
     this.state = {
       comments: [1, 2, 3, 4],
       scrollBoxHeight: null,
       isScrollHover: false,
+      scrollTop: 0,
     };
     this.onLoadMore = this.onLoadMore.bind(this);
     this.onScrollerWheel = this.onScrollerWheel.bind(this);
     this.onEnterScrolling = this.onEnterScrolling.bind(this);
     this.onLeaveScrolling = this.onLeaveScrolling.bind(this);
+    this.onScrollbarUpdate = this.onScrollbarUpdate.bind(this);
   }
 
   componentDidMount() {
-    // const style = window.getComputedStyle(this.scroller);
     this.onMount(this.scroller.clientHeight);
   }
 
@@ -31,79 +40,54 @@ class PublicComment extends React.Component {
 
   onScrollerWheel(e) {
     // TODO: Browser issue?
-    const scrollBoxTop = this.scrollOuter.scrollTop;
-    const { scrollBoxHeight } = this.state;
+    const { scrollBoxHeight, scrollTop } = this.state;
     const scrollerHeight = this.scroller.clientHeight;
-
     if (scrollBoxHeight === scrollerHeight) {
       return;
     }
-    if (e.deltaY < 0 && scrollBoxTop === 0) {
+    if (e.deltaY < 0 && scrollTop === 0) {
       e.preventDefault();
       return;
     }
-    if (e.deltaY > 0 && (scrollBoxTop + scrollBoxHeight) >= scrollerHeight) {
+    if (e.deltaY > 0 && scrollTop === 1) {
       e.preventDefault();
     }
   }
 
-  rCommentInner() {
-    return (
-      <div styleName="comment-inner">
-        <div styleName="avatar" />
-        <div styleName="content-wrapper">
-          <div styleName="header">
-            <span styleName="name">陳小明</span>
-            <span styleName="relative-time">23分鐘前</span>
-          </div>
-          <div styleName="content">
-            有沒有更清楚的規格可以看？
-            有沒有更清楚的規格可以看？
-            有沒有更清楚的規格可以看？有沒有更清楚的規格可以看？
-            有沒有更清楚的規格可以看？有沒有更清楚的規格可以看？有沒有更清楚的 ...顯示更多
-          </div>
-        </div>
-      </div>
-    );
-  }
-
   onEnterScrolling() {
-    console.log('scrolling');
     this.setState({ isScrollHover: true });
   }
 
   onLeaveScrolling() {
-    console.log('leave scrolling');
     this.setState({ isScrollHover: false });
+  }
+
+  onScrollbarUpdate(values) {
+    const { top } = values;
+    this.setState({ scrollTop: top });
   }
 
   render() {
     const { scrollBoxHeight, comments, isScrollHover } = this.state;
+    const { rScrollBar } = this.constructor;
     return (
       <div styleName="container">
         <h2 styleName="title">公開留言 | 86則</h2>
         <div styleName="box-border">
-          <div
+          <Scrollbars
+            onUpdate={this.onScrollbarUpdate}
+            renderThumbHorizontal={rScrollBar}
+            renderThumbVertical={rScrollBar}
+
             styleName={isScrollHover ? 'scrolling-box' : 'scroll-box'}
             style={{ height: scrollBoxHeight }}
-            ref={box => (this.scrollBox = box)}
+
             onMouseEnter={this.onEnterScrolling}
             onMouseLeave={this.onLeaveScrolling}
           >
-            <div
-              styleName="scroll-outer"
-              ref={outer => (this.scrollOuter = outer)}
-              onWheel={this.onScrollerWheel}
-            >
-              <div
-                styleName="scroll-inner"
-                ref={inner => (this.scroller = inner)}
-              >
-                {comments.map(n =>
-                  <div key={n} styleName="comment-row">
-                    {this.rCommentInner()}
-                  </div>,
-                )}
+            <div styleName="scroll-outer" onWheel={this.onScrollerWheel} >
+              <div styleName="scroll-inner" ref={n => (this.scroller = n)} >
+                {comments.map(n => <CommentRow key={n} />)}
                 <div styleName="comment-row-more">
                   <button styleName="more-btn" onClick={this.onLoadMore}>
                     查看更多留言
@@ -111,7 +95,7 @@ class PublicComment extends React.Component {
                 </div>
               </div>
             </div>
-          </div>
+          </Scrollbars>
         </div>
       </div>
     );
