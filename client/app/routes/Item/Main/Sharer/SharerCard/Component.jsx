bc0428b36f1f344a2fee7b994d9dacb5d2152f10
Nick <phyala@gmail.com>
2017-04-24 18:40:17 +0800

    Fix sharer card avatar css

--- client/app/routes/Item/Main/Sharer/SharerCard/Component.jsx
+++ client/app/routes/Item/Main/Sharer/SharerCard/Component.jsx
@@ -1,7 +1,9 @@
-import React, { PropTypes } from 'react'
+import React, { PropTypes } from 'react';
 
+const FAKER_IAMGE = 'http://r.fod4.com/c=sq/s=w1000,pd1/o=80/http://p.fod4.com/p/channels/qpztk/profile/ToJNnsiRhmm405cz7CPX_charlize-theron-head-shot.jpg';
 const SharerCard = () => (
   <div styleName="container">
+    <div styleName="avatar" style={{ backgroundImage: `url(${FAKER_IAMGE})` }} />
     <div styleName="details">
       <div styleName="name">
         <span>弗瑞德・塔塔薛瑞</span>
@@ -11,12 +13,21 @@ const SharerCard = () => (
         台北市中正區，於2017/01/20 加入
       </div>
       <div styleName="marks">
-        <div styleName="score"></div>
-        <div styleName="verifies"></div>
+        <div styleName="rating">
+          {[1, 2, 3, 4, 5].map(() =>
+            <span className="fa fa-star" styleName="star" />,
+          )}
+          <span styleName="star-text">評價</span>
+        </div>
+        <div styleName="verifies">
+          <span className="fa fa-mobile" styleName="mobile" />
+          <span className="fa fa-envelope" styleName="email" />
+          <span className="fa fa-facebook-square" styleName="facebook" />
+          <span styleName="verified-text">已評價</span>
+        </div>
       </div>
     </div>
-    <div styleName="avatar" />
   </div>
-)
+);
 
 export default SharerCard;
