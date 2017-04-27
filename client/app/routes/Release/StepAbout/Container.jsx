6af32c6383f147f1e9db517a745ddc7959478dc7
Nick <phyala@gmail.com>
2017-04-27 15:50:44 +0800

    Fix react slick matchmedia error on ie9

--- client/app/routes/Release/StepAbout/Container.jsx
+++ client/app/routes/Release/StepAbout/Container.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import CSS from 'react-css-modules';
+import styles from './styles.sass';
 
+const placeholders = {
+  name: '請輸入',
+  description: '清楚介紹您的物品，敘述更多吸引人的細節',
+};
 const Container = () => (
-  <div>
-    <h2>About</h2>
+  <div styleName="container">
+    <div styleName="inputControl">
+      <div styleName="inputHeader">
+        <label>物品名稱</label><span>0/30</span>
+      </div>
+      <input
+        styleName="inputField"
+        placeholder={placeholders.name}
+      />
+    </div>
+    <div styleName="inputControl">
+      <div styleName="inputHeader">
+        <label>物品描述</label><span>0/250</span>
+      </div>
+      <textarea
+        styleName="inputField"
+        placeholder={placeholders.description}
+      />
+    </div>
   </div>
 );
 
@@ -11,4 +34,4 @@ const mapStateToProps = (state) => {
   const { environment, routesHelper } = state;
   return ({ environment, routesHelper });
 };
-export default connect(mapStateToProps)(Container);
+export default connect(mapStateToProps)(CSS(Container, styles));
