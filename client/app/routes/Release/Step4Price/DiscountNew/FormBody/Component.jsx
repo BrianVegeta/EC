8bb764faa42e454d8f93d5d2ccca0a3b2d46bd91
Nick <phyala@gmail.com>
2017-05-08 15:22:56 +0800

    [#ITEM RELEASE] add input unit

--- client/app/routes/Release/Step4Price/DiscountNew/FormBody/Component.jsx
+++ client/app/routes/Release/Step4Price/DiscountNew/FormBody/Component.jsx
@@ -2,45 +2,38 @@ import React, { PropTypes } from 'react';
 import FormGroup from '../../../components/FormGroup';
 import InputCurrency from '../../../components/InputCurrency';
 import InputCounter from '../../../components/InputCounter';
+import InputUnit from '../../../components/InputUnit';
 
 class FormBody extends React.Component {
   render() {
+    const headerTextStyle = {
+      fontSize: 18,
+      fontWeight: 300,
+    };
+    const groupHalfStyle = {
+      width: '50%',
+      paddingRight: 10,
+      display: 'inline-block',
+    };
     return (
       <div>
         <FormGroup
           headerText="一次租用天數"
-          groupStyle={{
-            width: '50%',
-            paddingRight: 10,
-            display: 'inline-block',
-          }}
-          headerTextStyle={{
-            fontSize: 18,
-            fontWeight: 300,
-          }}
+          groupStyle={groupHalfStyle}
+          headerTextStyle={headerTextStyle}
         >
           <InputCounter value={1} suffix="天" />
         </FormGroup>
         <FormGroup
           headerText="優惠折扣"
-          groupStyle={{
-            width: '50%',
-            paddingLeft: 10,
-            display: 'inline-block',
-          }}
-          headerTextStyle={{
-            fontSize: 18,
-            fontWeight: 300,
-          }}
+          groupStyle={groupHalfStyle}
+          headerTextStyle={headerTextStyle}
         >
-          <InputCurrency value={1} />
+          <InputUnit placeholder="如：95折" suffix="折" />
         </FormGroup>
         <FormGroup
           headerText="選擇常用的分類"
-          headerTextStyle={{
-            fontSize: 18,
-            fontWeight: 300,
-          }}
+          headerTextStyle={headerTextStyle}
         >
           <InputCurrency value={1} />
         </FormGroup>
