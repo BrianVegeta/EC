0e5f25fe5d000e0e38b125567745e6cb51604a89
Nick <phyala@gmail.com>
2017-05-11 19:00:43 +0800

    [#ITEM RELEASE] add rc slider for image zoom

--- client/app/routes/Release/Step1Cover/ImageCropper/Controller/Component.jsx
+++ client/app/routes/Release/Step1Cover/ImageCropper/Controller/Component.jsx
@@ -1,6 +1,7 @@
 import React, { PropTypes } from 'react';
 import RotateRightIcon from 'react-icons/lib/md/rotate-right';
 import RotateLeftIcon from 'react-icons/lib/md/rotate-left';
+import Slider from 'rc-slider';
 
 class Controller extends React.Component {
   render() {
@@ -12,6 +13,9 @@ class Controller extends React.Component {
         <button styleName="rotateBtn" className="button">
           <RotateRightIcon size={30} />
         </button>
+        <div styleName="sliderContainer">
+          <Slider handleStyle={{ marginTop: -11, width: 25, height: 25 }} />
+        </div>
       </div>
     );
   }
