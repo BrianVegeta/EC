02e267036a73a104fa12abc38d05073c76a87f99
Nick or Kuan <phyala@gmail.com>
2017-08-07 13:12:32 +0800

    [#ORDER DETAIL]Add remove reducer on order detail leave

--- client/app/routes/OrderDetail/route.jsx
+++ client/app/routes/OrderDetail/route.jsx
@@ -1,19 +1,25 @@
-import { injectReducer } from 'reducers';
+import { injectReducer, removeReducer } from 'reducers';
 import { fetchOrder } from './modules/orderdetail';
 
+const key = 'orderdetail';
 export default store => ({
   path: '/p/order_detail/:cid',
+
   onEnter: (nextState) => {
-    console.log(nextState);
     const { cid } = nextState.params;
     store.dispatch(fetchOrder(cid));
   },
+
+  onLeave: () => {
+    removeReducer(store, { key });
+  },
+
   getComponent(_nextState, cb) {
     require.ensure([], (require) => {
       const Orderdetail = require('./containers/OrderdetailContainer').default;
       const reducer = require('./modules/orderdetail').default;
 
-      injectReducer(store, { key: 'orderdetail', reducer });
+      injectReducer(store, { key, reducer });
 
       cb(null, Orderdetail);
     }, 'orderdetail');
