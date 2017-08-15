ad99a740233803c14b11b4a4a8de0871f05be2eb
nick <phyala@gmail.com>
2017-08-15 14:16:43 +0800

    Move popup to modules

--- client/app/constants/popupTypes.jsx
+++
@@ -1,7 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-
-export const NEW_PASSWORD = 'NEW_PASSWORD';
-export const CHECK_PASSWORD = 'CHECK_PASSWORD';
-export const BANK_SETUP = 'BANK_SETUP';
-export const ACCESS_CHECK = 'ACCESS_CHECK';
-export const PUBLISH_ENTRY = 'PUBLISH_ENTRY';
