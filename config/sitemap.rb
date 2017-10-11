4dcead2bf06b75171504a210a1c60ed511f8a676
Nick's macbook <phyala@gmail.com>
2017-10-11 14:51:33 +0800

    Refresh sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,32 +1,87 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "https://web.shareapp.com.tw"
+require 'hirb'
+Hirb.enable
 
+
+class CateHelper
+  class << self
+    def collect data
+      categories = data.map do |v|
+        v['children'].map do |mv|
+          if mv['children']
+            [ CateHelper.slice(mv), mv['children'].map{ |sv| CateHelper.slice sv } ]
+          else
+            CateHelper.slice mv
+          end
+        end
+      end
+      categories.flatten!
+      categories.sort_by! { |v| v['id'] }
+      categories
+    end
+
+    def slice category
+      category.slice('id', 'name')
+    end
+  end
+end
+
+class ItemHelper
+  class << self
+    def slice item
+      item.slice('pid', 'pname')
+    end
+  end
+end
+
+class PathHelper
+  class << self
+    def category_path name, cid, used = false
+      "/p/i/#{used ? 'used' : 'lease'}/#{escape_alias(name)}-c.#{cid}"
+    end
+
+    def item pname, pid
+      "/p/#{escape_alias(pname)}-i.#{pid}"
+    end
+
+    private
+    def escape_alias text
+      text = text.gsub(/[^\u4e00-\u9fa5a-zA-Z0-9\-_]/, '-')
+      text.gsub(/((-))+/, '-')
+    end
+  end
+end
+
+SitemapGenerator::Sitemap.default_host = "https://web.shareapp.com.tw"
 SitemapGenerator::Sitemap.create do
-  # Put links creation logic here.
-  #
-  # The root path '/' and sitemap index file are added automatically for you.
-  # Links are added to the Sitemap in the order they are specified.
-  #
-  # Usage: add(path, options={})
-  #        (default options are used if you don't specify)
-  #
-  # Defaults: :priority => 0.5, :changefreq => 'weekly',
-  #           :lastmod => Time.now, :host => default_host
-  #
-  # Examples:
-  #
-  # Add '/articles'
-  #
-  #   add articles_path, :priority => 0.7, :changefreq => 'daily'
-  #
-  # Add all articles:
-  #
-  #   Article.find_each do |article|
-  #     add article_path(article), :lastmod => article.updated_at
-  #   end
-  add '/p/i/service', changefreq: 'daily'
-  add '/p/i/space', changefreq: 'daily'
-  add '/p/i/goods', changefreq: 'daily'
-  add '/p/i/used-goods', changefreq: 'daily'
-  add '/p/i/wishing-pond', changefreq: 'daily'
+
+  # TABS PAGES
+  add '/p/i/service', changefreq: 'daily', priority: 0.8
+  add '/p/i/space', changefreq: 'daily', priority: 0.8
+  add '/p/i/goods', changefreq: 'daily', priority: 0.8
+  add '/p/i/used-goods', changefreq: 'daily', priority: 0.8
+  add '/p/i/wishing-pond', changefreq: 'daily', priority: 0.8
+
+  # ITEMS
+  item_params = { index: 0, sort: { column: :time, type: :desc } }
+  api = ::Api::Item::SearchItemList.new item_params.merge size: 2000
+  if api.request
+    api.response_data.each do |item|
+      path = PathHelper.item item['pname'], item['pid']
+      add URI::escape(path), changefreq: 'monthly', priority: 0.75, lastmod: Time.now
+    end
+  end
+
+  # CATEGORIES
+  api_category = ::Options::Categories.new
+  if api_category.request
+    categories = CateHelper.collect api_category.response_data
+    categories.each do |category|
+      lease_path = PathHelper.category_path category['name'], category['id']
+      used_path = PathHelper.category_path category['name'], category['id']
+      add URI::escape(lease_path), changefreq: 'daily'
+      add URI::escape(used_path), changefreq: 'daily'
+    end
+  end
+
 end
